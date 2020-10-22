6c9d7101028479955e43cbe4c7d31c04c0342d26
Marco Antonio Trujano Rodriguez <trujas_2309@hotmail.com>
2020-10-22 18:45:32 -0500

    Terminado de ultimas

--- main.js
+++ main.js
@@ -1,23 +1,100 @@
-//GLOBAL VARIABLES
-let correctsAnswers = [];
+let correctsAnswers = []
+class Questions {
+    constructor(data) {
+        this.data = data
+    }
 
-// CONSUME TRIVIA API
+    //OBTAIN ALL ANSWERS AND APPLY A RANDOM POSITION
+    returnAnswersHTML(correct, incorrects, indexCard) {
+
+        incorrects.push(correct);
+        // This one sort the position of all answers
+        incorrects.sort(function () { return Math.random() - 0.5 });
+
+        //This option only sort the position of correcto answer
+        //incorrects.splice([Math.floor(Math.random() * 4)], 0, correct);
+
+        let answersHTML = '';
+        incorrects.forEach((incorrect, index) => {
+
+            answersHTML += `<div class="form-check">
+                                <input class="form-check-input" type="radio" name="radios-${indexCard}" id="radio${indexCard}-${index}" value="${incorrect}" required>
+                                <label class="form-check-label" for="radio${indexCard}-${index}">${incorrect}</label>
+                            </div>`;
+        });
+
+        return answersHTML;
+    }
+
+    //ESTRUCTURE OF CARD INFORMATION 
+    returnCardHTML(q, indexCard) {
+
+        const card = `<div class="card margin-card">
+                        <div class="card-body bg-card">
+                        <h5 class="card-title">${q.category}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
+                            ${this.returnAnswersHTML(q.correct_answer, q.incorrect_answers, indexCard)}           
+                        </div>
+                    </div>`;
+
+        return card;
+    }
+
+    // OBTAIN CORRECT ANSWER ARRAY
+    correCtAnswers(questions) {
+        let correctAns = [];
+        questions.forEach(question => correctAns.push(question.correct_answer));
+        //console.log(correctAns) //Don't show de answers in console.
+        return correctAns;
+    }
+
+    //DISPLAY CARDS
+    printCards() {
+        let questions = this.data
+        const container = document.getElementById('container-cards');
+        container.innerHTML = '';
+        let x = document.getElementById('btnWorked');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        }
+
+        if (questions.length === 0) {
+            let y = document.getElementById('btnWorked');
+            if (y.style.display === 'block') {
+                y.style.display = 'none';
+            }
+            container.innerHTML = `<div class="alert alert-warning" role="alert">
+                                        ¡Test again, this section is empty!
+                                    </div>`;
+        } else {
+            questions.forEach((question, indexCard) => {
+                const card = this.returnCardHTML(question, indexCard);
+                container.innerHTML += card;
+            });
+        }
+        correctsAnswers = this.correCtAnswers(questions);
+        //se pasa a front// container.innerHTML += `<button type="button" onclick="validate(correctAnswers)" id="btnWorked" class="btn btn-success m-5">Validar</button>`;
+    }
+
+    // OBTAIN HOW MANY CORRECT ANSWERS USER HAVE
+    
+
+}
+//
+
+
+// OBTAIN CATEGORIES
 function getCategory() {
     fetch(`https://opentdb.com/api_category.php`)
         .then(response => response.json())
         .then(data => {
-            category(data.trivia_categories)
-        });
-}
-
-// OBTAIN CATEGORIES
-function category(categories) {
-    const categoriesHTML = document.getElementById('category-type');
-    //const categorySelect = document.getElementById('category-type').value;
-    categories.forEach(category => {
-        const categoryType = `<option value="${category.id}">${category.name}</option>`;
-        categoriesHTML.innerHTML += categoryType;
-    });
+            categories = data.trivia_categories
+            const categoriesHTML = document.getElementById('category-type');
+            categories.forEach(category => {
+                const categoryType = `<option value="${category.id}">${category.name}</option>`;
+                categoriesHTML.innerHTML += categoryType;
+            });
+        })
 }
 
 // OBTAIN VALUE FOR EACH FORM CONTROL 
@@ -29,97 +106,12 @@ function getQuestions() {
     fetch(`https://opentdb.com/api.php?amount=${questionsQuantity}&category=${categorySelect}&difficulty=${difficulty}&type=${type}`)
         .then(response => response.json())
         .then(data => {
-            printCards(data.results)
-        });
+            let trivia = new Questions(data.results)
+            trivia.printCards()
+        })   
 }
 
-//OBTAIN ALL ANSWERS AND APPLY A RANDOM POSITION
-function returnAnswersHTML(correct, incorrects, indexCard) {
-
-    incorrects.push(correct);
-    // This one sort the position of all answers
-    incorrects.sort(function () { return Math.random() - 0.5 });
-
-    //This option only sort the position of correcto answer
-    //incorrects.splice([Math.floor(Math.random() * 4)], 0, correct);
-
-    let answersHTML = '';
-    incorrects.forEach((incorrect, index) => {
-
-        answersHTML += `<div class="form-check">
-                            <input class="form-check-input" type="radio" name="radios-${indexCard}" id="radio${indexCard}-${index}" value="${incorrect}" required>
-                            <label class="form-check-label" for="radio${indexCard}-${index}">${incorrect}</label>
-                        </div>`;
-    });
-
-    return answersHTML;
-}
-
-//ESTRUCTURE OF CARD INFORMATION 
-function returnCardHTML(q, indexCard) {
-
-    const card = `<div class="card margin-card">
-                    <div class="card-body bg-card">
-                    <h5 class="card-title">${q.category}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
-                        ${returnAnswersHTML(q.correct_answer, q.incorrect_answers, indexCard)}           
-                    </div>
-                </div>`;
-
-    return card;
-}
-
-// OBTAIN CORRECT ANSWER ARRAY
-function correctAnswers(questions) {
-    let correctAns = [];
-    questions.forEach(question => correctAns.push(question.correct_answer));
-    //console.log(correctAns) //Don't show de answers in console.
-    return correctAns;
-}
-
-//DISPLAY CARDS
-function printCards(questions) {
-
-    x = document.getElementById('btnWorked');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    }
-
-    const container = document.getElementById('container-cards');
-    container.innerHTML = '';
-    if (questions.length === 0) {
-        y = document.getElementById('btnWorked');
-        if (y.style.display === 'block') {
-            y.style.display = 'none';
-        }
-        container.innerHTML = `<div class="alert alert-warning" role="alert">
-                                    ¡Test again, this section is empty!
-                                </div>`;
-    } else {
-        questions.forEach((question, indexCard) => {
-            const card = returnCardHTML(question, indexCard);
-            container.innerHTML += card;
-        });
-    }
-    correctsAnswers = correctAnswers(questions);
-    //se pasa a front// container.innerHTML += `<button type="button" onclick="validate(correctAnswers)" id="btnWorked" class="btn btn-success m-5">Validar</button>`;
-}
-
-// OBTAIN HOW MANY CORRECT ANSWERS USER HAVE
 function validate() {
-    // const btnVAl = document.getElementById('btnWorked').value
-    // console.log( btnVAl)
-    // const numCards = document.getElementById('questions-number').value
-
-    // const elements = document.getElementsByClassName('.col')
-    // Array.from(elements)
-    // document.querySelectorAll('name*=radios-')
-    // document.querySelector('.col')
-
-    /*CA =[true, false]
-    [radio1, radio2]
-    radio1.value == true*/
-
     let numGood = 0;
     for (let i = 0; i < correctsAnswers.length; i++) {
         const options = document.getElementsByName(`radios-${i}`);
@@ -140,5 +132,5 @@ function validate() {
     }
 }
 
-getCategory();
-window.category = category
+getCategory()
+// 
